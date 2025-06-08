a31844f96b9e985ff374be1da03681f694f24179
Eyobed <eyobedteshome@gmail.com>
2025-06-08 22:54:37 +0300

    arithmetic_operation updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 result = "Cannot divide by zero."
-            else:
+            elif num != 0:
                 result = num1 / num2
         case _:
             result = "Invalid operation."
