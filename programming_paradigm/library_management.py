bc384db5581589347ff3468465c9574a6a826fd5
Eyobed <eyobedteshome@gmail.com>
2025-06-13 10:46:53 +0300

    library management added

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,36 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+        
+    def isCheckedOut(self):
+        return self._is_checked_out
+    
+    def check_out(self):
+        self._is_checked_out = True
+        
+    def returned(self):
+        self._is_checked_out = False
+        
+class Library:
+    def __init__(self):
+        self._books = []
+        
+    def add_book(self, Book):
+        self._books.append(Book)
+        
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book.check_out()
+                
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and book.isCheckedOut():
+                book.returned()
+                
+    def list_available_books(self):
+        for book in self._books:
+            if not book.isCheckedOut():
+                print(f"{book.title} by {book.author}")
